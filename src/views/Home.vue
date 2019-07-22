14ca907bb175ab1e15ea45bd6ba3569ad7175b38
wenjun ye <1728914873@qq.com>
2019-07-22 09:56:19 +0800

    fix conflict

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,7 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-<<<<<<< HEAD
     <HelloWorld msg="Hello World"/>
-=======
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
-    <p>git pull 测试</p>
->>>>>>> 12939f54da99bab16a77d15819303c790eff068e
   </div>
 </template>
 
